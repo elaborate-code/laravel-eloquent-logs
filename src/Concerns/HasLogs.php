1bba4878dd1eff00fd9af778d4ae859ffa7bbc1c
medilies <medilies@gmail.com>
2022-08-04 20:43:21 +0100

    Refactor: trait methods

--- src/Concerns/HasLogs.php
+++ src/Concerns/HasLogs.php
@@ -5,57 +5,35 @@ namespace ElaborateCode\EloquentLogs\Concerns;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 trait HasLogs
 {
-    public bool $loggableIsBeingRestored = false;
+    public bool $loggableBeingRestoredFlag = false;
+
+    public ?array $loggableOptions = null;
 
     public static function bootHasLogs(): void
     {
-        self::created(callback: function ($model) {
-            self::log($model, 'created');
-        });
+        self::created([self::class, 'createdHandler']);
 
-        self::updated(callback: function ($model) {
-            if (isset($model->loggableIsBeingRestored) && $model->loggableIsBeingRestored) {
-                // This is a restored event so don't log!
-                return;
-            }
+        self::updated([self::class, 'updatedHandler']);
 
-            self::log($model, 'updated');
-        });
-
-        self::deleted(callback: function ($model) {
-            if (in_array('Illuminate\Database\Eloquent\SoftDeletes', (class_uses(self::class)))) {
-                // This is a softDeleted or a forceDeleted event so don't log!
-                return;
-            }
-
-            self::log($model, 'deleted');
-        });
+        self::deleted([self::class, 'deletedHandler']);
 
         if (! in_array('Illuminate\Database\Eloquent\SoftDeletes', (class_uses(self::class)))) {
             // This model will not fire restore, soft delete and force delete events!
             return;
         }
 
-        self::softDeleted(callback: function ($model) {
-            self::log($model, 'soft deleted');
-        });
+        self::softDeleted([self::class, 'softDeletedHandler']);
 
-        self::forceDeleted(callback: function ($model) {
-            self::log($model, 'force deleted');
-        });
+        self::forceDeleted([self::class, 'forceDeletedHandler']);
 
-        self::restored(callback: function ($model) {
-            self::log($model, 'restored');
+        self::restored([self::class, 'restoredHandler']);
 
-            $model->loggableIsBeingRestored = false;
-        });
-
-        self::restoring(callback: function ($model) {
-            $model->loggableIsBeingRestored = true;
-        });
+        // A work around Laravel firing many events when restored
+        self::restoring([self::class, 'setLoggableBeingRestoredFlag']);
     }
 
     public static function log(Model $model, string $event): void
@@ -66,8 +44,113 @@ trait HasLogs
         ]);
     }
 
+    /*
+    |--------------------------------------------------------------------------
+    | Handlers
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    /**
+     * Handles the created event
+     */
+    public static function createdHandler(Model $model): void
+    {
+        self::log($model, self::eventNameFromHandler(__FUNCTION__));
+    }
+
+    /**
+     * Handles the updated event
+     */
+    public static function updatedHandler(Model $model): void
+    {
+        if (self::isLoggableBeingRestored($model)) {
+            // This is a restored event so don't log!
+            return;
+        }
+
+        self::log($model, self::eventNameFromHandler(__FUNCTION__));
+    }
+
+    /**
+     * Handles the deleted event
+     */
+    public static function deletedHandler(Model $model): void
+    {
+        if (in_array('Illuminate\Database\Eloquent\SoftDeletes', (class_uses(self::class)))) {
+            // This is a softDeleted or a forceDeleted event so don't log!
+            return;
+        }
+
+        self::log($model, self::eventNameFromHandler(__FUNCTION__));
+    }
+
+    /**
+     * Handles the softDeleted event
+     */
+    public static function softDeletedHandler(Model $model): void
+    {
+        self::log($model, self::eventNameFromHandler(__FUNCTION__));
+    }
+
+    /**
+     * Handles the forceDeleted event
+     */
+    public static function forceDeletedHandler(Model $model): void
+    {
+        self::log($model, self::eventNameFromHandler(__FUNCTION__));
+    }
+
+    /**
+     * Handles the restored event
+     */
+    public static function restoredHandler(Model $model): void
+    {
+        self::log($model, self::eventNameFromHandler(__FUNCTION__));
+
+        self::resetLoggableBeingRestoredFlag($model);
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relationships
+    |--------------------------------------------------------------------------
+    |
+    */
+
     public function eloquentLogs(): MorphMany
     {
         return $this->morphMany(config('eloquent-logs.logs_model') ?? ElaborateCode\EloquentLogs\Models\EloquentLog::class, 'loggable');
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Helpers
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    /**
+     * Transforms the string input to lower case separated by spaces
+     * and rejects the 'Handler' substring if found
+     */
+    public static function eventNameFromHandler(string $str): string
+    {
+        return Str::snake(str_replace('Handler', '', $str), ' ');
+    }
+
+    public static function setLoggableBeingRestoredFlag(Model $model): void
+    {
+        $model->loggableBeingRestoredFlag = true;
+    }
+
+    public static function resetLoggableBeingRestoredFlag(Model $model): void
+    {
+        $model->loggableBeingRestoredFlag = false;
+    }
+
+    public static function isLoggableBeingRestored(Model $model): bool
+    {
+        return $model->loggableBeingRestoredFlag;
     }
 }
